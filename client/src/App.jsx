e8f55a5ada185c0dee3af0dc2669a8ef8bdb7325
bilal <bilalrasheedw@gmail.com>
2023-08-21 20:26:31 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Heading>hello this is myntra site</Heading>
+    </div>
+  );
+};
+
+export default App;
